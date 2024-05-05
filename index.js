f532c83ead9ebaed95898be13c41b4028fb50fa1
OHshajim <ajshajimmax@gmail.com>
2024-05-06 02:46:03 +0600

    get jobs

--- index.js
+++ index.js
@@ -28,14 +28,22 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // jobs 
+        const jobsCollections = client.db('SoloSphere').collection('jobs')
+        app.get('/jobs', async (req, res) => {
+            const result = await jobsCollections.find().toArray();
+            res.send(result);
+        })
+
+        // bids 
+        const bidsCollections = client.db('SoloSphere').collection('bids')
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        // await client.close();
     }
 }
 run().catch(console.dir);
